77fdc8350e642285378b5cb11327cca80aec7928
Edgars Vasiljevs <edgars.vasiljevs@redboxdigital.com>
2019-07-11 17:14:21 +0300

    List all observers for all events + option to exclude core magento observers

--- src/ObserverList/ListCommand.php
+++ src/ObserverList/ListCommand.php
@@ -5,6 +5,7 @@ namespace ObserverList;
 use Mage;
 use N98\Magento\Command\AbstractMagentoCommand;
 use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Helper\Table;
@@ -32,8 +33,9 @@ class ListCommand extends AbstractMagentoCommand
     protected function configure()
     {
         $this->setName('scandi:observer-list')
-             ->setDescription('Show all observers for specified event')
-             ->addArgument('event', InputArgument::REQUIRED, 'Event code');
+             ->setDescription('Show all observers for all events or for specified event')
+             ->addArgument('event', InputArgument::OPTIONAL, 'Event code')
+             ->addOption('exclude-core', null, InputOption::VALUE_NONE, 'Exclude Magento core observers');
     }
 
    /**
@@ -52,20 +54,80 @@ class ListCommand extends AbstractMagentoCommand
             return;
         }
 
-        $this->_listObservers();
+        $event = $this->_input->getArgument('event');
+        $excludeCore = $this->_input->getOption('exclude-core');
+
+        if (!$event) {
+            $header = array('Event', 'Area', 'Type', 'Class', 'Method');
+            $rows = $this->_getAllObservers($excludeCore);
+            usort($rows, function ($a, $b) {
+                return strcmp($a[0], $b[0]);
+            });
+        }
+        else {
+            $header = array('Area', 'Type', 'Class', 'Method');
+            $rows = $this->_getEventObservers($event, $excludeCore);
+        }
+
+        $table = new Table($this->_output);
+        $table->setHeaders($header);
+        $table->setRows($rows);
+        $table->render();
+    }
+
+    /**
+     * Get all observers for all events
+     *
+     * @param bool $excludeCore
+     * @return array
+     */
+    protected function _getAllObservers($excludeCore = false)
+    {
+        $rows = array();
+
+        foreach($this->_areas as $area) {
+            $events = Mage::getConfig()->getNode($area . '/events');
+
+            if ($events === false) {
+                continue;
+            }
+
+            $events = $events->asArray();
+
+            foreach($events as $key => $item) {
+                foreach($item['observers'] as $observer) {
+
+                    $className = Mage::getConfig()->getModelClassName($observer['class']);
+
+                    if ($excludeCore && $this->_isCoreMagentoObserver($className)) {
+                        continue;
+                    }
+
+                    $rows[] = array(
+                        $key,
+                        $area,
+                        $observer['type'],
+                        $className,
+                        $observer['method'],
+                    );
+                }
+            }
+        }
+
+        return $rows;
     }
 
     /**
      * List observers by event code
+     *
+     * @param string $event
+     * @param bool $excludeCore
+     * @return array
      */
-    protected function _listObservers()
+    protected function _getEventObservers($event, $excludeCore = false)
     {
-        $event = $this->_input->getArgument('event');
-
-        $table = new Table($this->_output);
-        $table->setHeaders(array('Area', 'Type', 'Class', 'Method'));
-
         $rows = array();
+
         foreach($this->_areas as $area) {
             $eventConfig = Mage::getConfig()->getEventConfig($area, $event);
 
@@ -77,6 +139,10 @@ class ListCommand extends AbstractMagentoCommand
                 $class = $obsConfig->class ? (string)$obsConfig->class : $obsConfig->getClassName();
                 $className = Mage::getConfig()->getModelClassName($class);
 
+                if ($excludeCore && $this->_isCoreMagentoObserver($className)) {
+                    continue;
+                }
+
                 $rows[] = array(
                     $area,
                     (string)$obsConfig->type,
@@ -86,7 +152,15 @@ class ListCommand extends AbstractMagentoCommand
             }
         }
 
-        $table->setRows($rows);
-        $table->render();
+        return $rows;
+    }
+
+    /**
+     * @param $className
+     * @return bool
+     */
+    protected function _isCoreMagentoObserver($className)
+    {
+        return substr($className, 0, 5) == 'Mage_' || substr($className, 0, 11) == 'Enterprise_';
     }
 }
